a905c6c97389104e4dd56e337b98dd92ae88818a
Dennis Chan <dennischan@ucla.edu>
2016-04-25 21:43:00 -0700

    Add Flow message

--- protobuf/update_pb2.py
+++ protobuf/update_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='update.proto',
   package='sdn',
-  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"u\n\x06Update\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12%\n\x05utype\x18\x02 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\n\n\x02ip\x18\x03 \x02(\t\"%\n\nUpdateType\x12\t\n\x05QUERY\x10\x00\x12\x0c\n\x08RESPONSE\x10\x01\"#\n\x08Neighbor\x12\n\n\x02ip\x18\x01 \x02(\t\x12\x0b\n\x03rtt\x18\x02 \x01(\t\"=\n\x06Report\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12 \n\tneighbors\x18\x02 \x03(\x0b\x32\r.sdn.Neighbor')
+  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"u\n\x06Update\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12%\n\x05utype\x18\x02 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\n\n\x02ip\x18\x03 \x02(\t\"%\n\nUpdateType\x12\t\n\x05QUERY\x10\x00\x12\x0c\n\x08RESPONSE\x10\x01\"#\n\x08Neighbor\x12\n\n\x02ip\x18\x01 \x02(\t\x12\x0b\n\x03rtt\x18\x02 \x01(\t\"\x18\n\x04\x46low\x12\x10\n\x08jsonData\x18\x01 \x02(\t\"V\n\x06Report\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12 \n\tneighbors\x18\x02 \x03(\x0b\x32\r.sdn.Neighbor\x12\x17\n\x04\x66low\x18\x03 \x01(\x0b\x32\t.sdn.Flow')
 
 
 
@@ -117,6 +117,34 @@ _NEIGHBOR = _descriptor.Descriptor(
 )
 
 
+_FLOW = _descriptor.Descriptor(
+  name='Flow',
+  full_name='sdn.Flow',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='jsonData', full_name='sdn.Flow.jsonData', index=0,
+      number=1, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=177,
+  serialized_end=201,
+)
+
+
 _REPORT = _descriptor.Descriptor(
   name='Report',
   full_name='sdn.Report',
@@ -138,6 +166,13 @@ _REPORT = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='flow', full_name='sdn.Report.flow', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -147,15 +182,17 @@ _REPORT = _descriptor.Descriptor(
   options=None,
   is_extendable=False,
   extension_ranges=[],
-  serialized_start=177,
-  serialized_end=238,
+  serialized_start=203,
+  serialized_end=289,
 )
 
 _UPDATE.fields_by_name['utype'].enum_type = _UPDATE_UPDATETYPE
 _UPDATE_UPDATETYPE.containing_type = _UPDATE;
 _REPORT.fields_by_name['neighbors'].message_type = _NEIGHBOR
+_REPORT.fields_by_name['flow'].message_type = _FLOW
 DESCRIPTOR.message_types_by_name['Update'] = _UPDATE
 DESCRIPTOR.message_types_by_name['Neighbor'] = _NEIGHBOR
+DESCRIPTOR.message_types_by_name['Flow'] = _FLOW
 DESCRIPTOR.message_types_by_name['Report'] = _REPORT
 
 class Update(_message.Message):
@@ -169,6 +206,12 @@ class Neighbor(_message.Message):
   DESCRIPTOR = _NEIGHBOR
 
   # @@protoc_insertion_point(class_scope:sdn.Neighbor)
+
+class Flow(_message.Message):
+  __metaclass__ = _reflection.GeneratedProtocolMessageType
+  DESCRIPTOR = _FLOW
+
+  # @@protoc_insertion_point(class_scope:sdn.Flow)
 
 class Report(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
