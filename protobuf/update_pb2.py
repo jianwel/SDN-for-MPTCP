02197bc2b7cfe391d3ee195cdd17643956c2e4ea
Dennis Chan <dennischan@ucla.edu>
2016-05-16 21:36:13 -0700

    Add RouteChange message

--- protobuf/update_pb2.py
+++ protobuf/update_pb2.py
@@ -13,7 +13,7 @@ from google.protobuf import descriptor_pb2
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='update.proto',
   package='sdn',
-  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"\x84\x01\n\x06Update\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12%\n\x05utype\x18\x02 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\n\n\x02ip\x18\x03 \x02(\t\x12\r\n\x05\x61lias\x18\x04 \x01(\t\"%\n\nUpdateType\x12\t\n\x05QUERY\x10\x00\x12\x0c\n\x08RESPONSE\x10\x01\"E\n\x08Neighbor\x12\n\n\x02ip\x18\x01 \x02(\t\x12\x0b\n\x03rtt\x18\x02 \x01(\t\x12\r\n\x05\x61lias\x18\x03 \x01(\t\x12\x11\n\tinterface\x18\x04 \x01(\t\"\x7f\n\x06Report\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12 \n\tneighbors\x18\x02 \x03(\x0b\x32\r.sdn.Neighbor\x12\r\n\x05\x66lows\x18\x03 \x01(\t\x12\x0e\n\x06routes\x18\x04 \x01(\t\x12\r\n\x05\x61lias\x18\x05 \x01(\t\x12\x12\n\ninterfaces\x18\x06 \x01(\t')
+  serialized_pb='\n\x0cupdate.proto\x12\x03sdn\"\x84\x01\n\x06Update\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12%\n\x05utype\x18\x02 \x02(\x0e\x32\x16.sdn.Update.UpdateType\x12\n\n\x02ip\x18\x03 \x02(\t\x12\r\n\x05\x61lias\x18\x04 \x01(\t\"%\n\nUpdateType\x12\t\n\x05QUERY\x10\x00\x12\x0c\n\x08RESPONSE\x10\x01\"E\n\x08Neighbor\x12\n\n\x02ip\x18\x01 \x02(\t\x12\x0b\n\x03rtt\x18\x02 \x01(\t\x12\r\n\x05\x61lias\x18\x03 \x01(\t\x12\x11\n\tinterface\x18\x04 \x01(\t\"\x7f\n\x06Report\x12\x11\n\ttimestamp\x18\x01 \x02(\t\x12 \n\tneighbors\x18\x02 \x03(\x0b\x32\r.sdn.Neighbor\x12\r\n\x05\x66lows\x18\x03 \x01(\t\x12\x0e\n\x06routes\x18\x04 \x01(\t\x12\r\n\x05\x61lias\x18\x05 \x01(\t\x12\x12\n\ninterfaces\x18\x06 \x01(\t\"\x91\x01\n\x0bRouteChange\x12)\n\x07\x63ommand\x18\x01 \x02(\x0e\x32\x18.sdn.RouteChange.Command\x12\x13\n\x0b\x64\x65stination\x18\x02 \x02(\t\x12\x11\n\tinterface\x18\x03 \x02(\t\x12\x0f\n\x07gateway\x18\x04 \x01(\t\"\x1e\n\x07\x43ommand\x12\x07\n\x03\x41\x44\x44\x10\x00\x12\n\n\x06\x44\x45LETE\x10\x01')
 
 
 
@@ -36,6 +36,27 @@ _UPDATE_UPDATETYPE = _descriptor.EnumDescriptor(
   options=None,
   serialized_start=117,
   serialized_end=154,
+)
+
+_ROUTECHANGE_COMMAND = _descriptor.EnumDescriptor(
+  name='Command',
+  full_name='sdn.RouteChange.Command',
+  filename=None,
+  file=DESCRIPTOR,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='ADD', index=0, number=0,
+      options=None,
+      type=None),
+    _descriptor.EnumValueDescriptor(
+      name='DELETE', index=1, number=1,
+      options=None,
+      type=None),
+  ],
+  containing_type=None,
+  options=None,
+  serialized_start=472,
+  serialized_end=502,
 )
 
 
@@ -200,12 +221,65 @@ _REPORT = _descriptor.Descriptor(
   serialized_end=354,
 )
 
+
+_ROUTECHANGE = _descriptor.Descriptor(
+  name='RouteChange',
+  full_name='sdn.RouteChange',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='command', full_name='sdn.RouteChange.command', index=0,
+      number=1, type=14, cpp_type=8, label=2,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='destination', full_name='sdn.RouteChange.destination', index=1,
+      number=2, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='interface', full_name='sdn.RouteChange.interface', index=2,
+      number=3, type=9, cpp_type=9, label=2,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='gateway', full_name='sdn.RouteChange.gateway', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=unicode("", "utf-8"),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+    _ROUTECHANGE_COMMAND,
+  ],
+  options=None,
+  is_extendable=False,
+  extension_ranges=[],
+  serialized_start=357,
+  serialized_end=502,
+)
+
 _UPDATE.fields_by_name['utype'].enum_type = _UPDATE_UPDATETYPE
 _UPDATE_UPDATETYPE.containing_type = _UPDATE;
 _REPORT.fields_by_name['neighbors'].message_type = _NEIGHBOR
+_ROUTECHANGE.fields_by_name['command'].enum_type = _ROUTECHANGE_COMMAND
+_ROUTECHANGE_COMMAND.containing_type = _ROUTECHANGE;
 DESCRIPTOR.message_types_by_name['Update'] = _UPDATE
 DESCRIPTOR.message_types_by_name['Neighbor'] = _NEIGHBOR
 DESCRIPTOR.message_types_by_name['Report'] = _REPORT
+DESCRIPTOR.message_types_by_name['RouteChange'] = _ROUTECHANGE
 
 class Update(_message.Message):
   __metaclass__ = _reflection.GeneratedProtocolMessageType
@@ -224,6 +298,12 @@ class Report(_message.Message):
   DESCRIPTOR = _REPORT
 
   # @@protoc_insertion_point(class_scope:sdn.Report)
+
+class RouteChange(_message.Message):
+  __metaclass__ = _reflection.GeneratedProtocolMessageType
+  DESCRIPTOR = _ROUTECHANGE
+
+  # @@protoc_insertion_point(class_scope:sdn.RouteChange)
 
 
 # @@protoc_insertion_point(module_scope)
