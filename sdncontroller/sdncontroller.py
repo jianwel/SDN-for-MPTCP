3da64a7491621cfe05406220092492df3fbf8c03
Dennis Chan <dennischan@ucla.edu>
2016-05-28 15:45:36 -0700

    Allow the server socket to be reused

--- sdncontroller/sdncontroller.py
+++ sdncontroller/sdncontroller.py
@@ -397,6 +397,7 @@ def receive_worker(conn, addr, network, done_event):
 def server_worker(done_event, network, args):
   # Initialize server for receiving updates.
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   s.bind((args.controller_ip, args.controller_port))
   s.settimeout(5)
   s.listen(5)
